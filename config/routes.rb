446dfb444ccdcf1378bcdfd3ad8665e1c0b9a3ce
Kaomaruko <kaori.mikan1111@gmail.com>
2023-12-28 15:35:44 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
+  resources :users, only: [:edit, :update]
   # Defines the root path route ("/")
   root to: "messages#index"
 end
